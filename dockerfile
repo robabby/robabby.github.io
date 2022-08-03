b519fb11989f53b2524f44ed4fcd45f6dfe37dc4
Rob <robabby23@gmail.com>
2022-08-02 22:02:04 -0500

    Remove Gatsby, Update & Cleanup Deps

--- dockerfile
+++ dockerfile
@@ -1,4 +1,4 @@
-FROM node:12.3.1
+FROM node:16.16-alpine
 
 RUN npm i -g yarn
 
--- gatsby-ssr.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react"
-import { renderToString } from "react-dom/server"
-
-export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
-  const ConnectedBody = () => bodyComponent
-
-  replaceBodyHTMLString(renderToString(<ConnectedBody />))
-}
